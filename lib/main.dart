1f21ba3853484b58bd1a00a8aa1a4417104628d3
Steven Berkowitz <stevenaaronberkowitz@gmail.com>
2020-12-22 16:45:49 +0900

    added basic filtering based on issue open/closed or date created

--- lib/main.dart
+++ lib/main.dart
@@ -77,6 +77,12 @@ class _LabeledIssuesState extends State<LabeledIssuesView>
 
   String _label;
   var _issues = <Issue>[];
+  var _filteredIssues = <Issue>[];
+  // filter issues
+  bool showOpenIssues = true;
+  bool showClosedIssues = false;
+  bool showOldIssues = true;
+
   // TODO - add user login
   var github = GitHub(
       auth:
@@ -91,48 +97,122 @@ class _LabeledIssuesState extends State<LabeledIssuesView>
   }
 
   void loadIssuesWith({String label}) {
-    github.issues.listByRepo(RepositorySlug("flutter", "flutter"),
-        labels: [_label]).listen((event) {
-      setState(() {
-        _issues.add(event);
-        print(_label);
-      });
+    github.issues
+        .listByRepo(RepositorySlug("flutter", "flutter"),
+            state: "all", labels: [_label])
+        .take(100)
+        .listen((event) {
+          setState(() {
+            _issues.add(event);
+            _filteredIssues.add(event);
+            print(_label);
+          });
+        });
+  }
+
+  // filter the issues using options from showFilterOptions
+  // update _filteredIssues and set state
+  void filterIssues() {
+    print("filter issues $_label");
+    print("show open issues  :  $showOpenIssues");
+    print("show closed issues: $showClosedIssues");
+    DateTime filterTime = showOldIssues
+        ? DateTime.utc(1977, 7, 7)
+        : DateTime.now().subtract(Duration(days: 365));
+    List filteredIssues = _issues
+        .where((f) => f.isOpen == showOpenIssues)
+        .where((f) => f.isClosed == showClosedIssues)
+        .where((f) => f.createdAt.isAfter(filterTime))
+        .toList();
+    // filteredIssues.sort()
+    print(filteredIssues.length);
+    setState(() {
+      print("setting filtered issues");
+      print(_filteredIssues.length);
+      _filteredIssues = filteredIssues;
+      print(_filteredIssues.length);
     });
   }
 
-  void showDialogBox(BuildContext context) {
-    // flutter defined function
+  void showFilterOptions(BuildContext context) {
     showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        // return object of type Dialog
-        return AlertDialog(
-          title: new Text("Apply Filters"),
-          // content: new Text("Alert Dialog body"),
-          actions: [
-            // Filter
-            // TODO - filter closed issues
-            // TODO - filter issues older than a year
-            // Sort
-            // TODO - sort by created
-            // TODO - sort by last edited
-            // TODO - sort by comment count
-            new FlatButton(
-              child: new Text("Close"),
-              onPressed: () {
-                Navigator.of(context).pop();
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Apply Filters"),
+            content: StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // Filter
+                    // TODO - filter closed issues
+                    Row(
+                      children: [
+                        Text("Show open issues"),
+                        Checkbox(
+                          value: showOpenIssues,
+                          onChanged: (bool internalValue) {
+                            setState(() {
+                              showOpenIssues = !showOpenIssues;
+                            });
+                          },
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text("Show closed issues"),
+                        Checkbox(
+                          value: showClosedIssues,
+                          onChanged: (bool internalValue) {
+                            setState(() {
+                              showClosedIssues = !showClosedIssues;
+                            });
+                          },
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text("show old issues"),
+                        Checkbox(
+                          value: showOldIssues,
+                          onChanged: (bool internalValue) {
+                            setState(() {
+                              showOldIssues = !showOldIssues;
+                            });
+                          },
+                        )
+                      ],
+                    ),
+
+                    // TODO - filter issues older than a year
+                    // Sort
+                    // TODO - sort by created
+                    // TODO - sort by last edited
+                    // TODO - sort by comment count
+
+                    FlatButton(
+                      child: Text("Close"),
+                      onPressed: () {
+                        filterIssues();
+
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ],
+                );
               },
             ),
-          ],
-        );
-      },
-    );
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return _issues.isNotEmpty ? _issueListView() : _progressView();
+    return _filteredIssues.isNotEmpty ? _issueListView() : _progressView();
   }
 
   Widget _issueListView() {
@@ -141,11 +221,12 @@ class _LabeledIssuesState extends State<LabeledIssuesView>
         Expanded(
             child: ListView.builder(
                 padding: EdgeInsets.all(16.0),
-                itemCount: _issues.length,
+                itemCount: _filteredIssues.length,
                 itemBuilder: (context, i) {
                   if (i.isOdd) return Divider();
                   final index = i ~/ 2;
-                  String text = _issues[index].title + " " + index.toString();
+                  String text =
+                      _filteredIssues[index].title + " " + index.toString();
                   return Text(text);
                 })),
         Padding(
@@ -153,7 +234,7 @@ class _LabeledIssuesState extends State<LabeledIssuesView>
           child: ElevatedButton(
             child: Text("filter"),
             onPressed: () {
-              showDialogBox(context);
+              showFilterOptions(context);
             },
           ),
         )
@@ -164,5 +245,21 @@ class _LabeledIssuesState extends State<LabeledIssuesView>
   Widget _progressView() {
     // TODO - add progress indicator
     return Text("progress view here");
+  }
+
+  Widget _checkboxColumn(String label, bool value) {
+    return Row(
+      children: [
+        Text(label),
+        Checkbox(
+          value: value,
+          onChanged: (bool internalValue) {
+            setState(() {
+              value = !value;
+            });
+          },
+        )
+      ],
+    );
   }
 }
