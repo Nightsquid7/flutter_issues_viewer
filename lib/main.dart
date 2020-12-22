5de974ffc3f8fd366b63970e8541125dab07f512
Steven Berkowitz <stevenaaronberkowitz@gmail.com>
2020-12-22 11:43:38 +0900

    added tabs with labels & loading github issues for each label

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
 import 'package:github/github.dart';
 
 void main() {
@@ -27,92 +28,108 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LabeledIssuesTabBarController(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class LabeledIssuesTabBarController extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 6,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(text: "全て"),
+                Tab(text: "p: webview"),
+                Tab(text: "p: shared_preferences"),
+                Tab(text: "waiting for customer response"),
+                Tab(text: "severe: new feature"),
+                Tab(text: "p: share")
+              ],
+            ),
+            title: Text("Flutter Issues"),
+          ),
+          body: TabBarView(
+            children: [
+              LabeledIssuesView(
+                label: "",
+              ),
+              LabeledIssuesView(
+                label: "p: webview",
+              ),
+              LabeledIssuesView(
+                label: "p: shared_preferences",
+              ),
+              LabeledIssuesView(
+                label: "waiting for customer response",
+              ),
+              LabeledIssuesView(
+                label: "severe: new feature",
+              ),
+              LabeledIssuesView(
+                label: "p: share",
+              )
+            ],
+          ),
+        ));
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class LabeledIssuesView extends StatefulWidget {
+  final String label;
+  LabeledIssuesView({Key key, this.label}) : super(key: key);
+  _LabeledIssuesState createState() => _LabeledIssuesState();
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _LabeledIssuesState extends State<LabeledIssuesView>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  String _label;
+
+  var _issues = <Issue>[];
+  var github = GitHub(
+      auth:
+          Authentication.withToken("3cdeeb6e0861301a3b14d3e8ba94173d5eaa1869"));
+  int pageCount = 10;
+
+  @override
+  void initState() {
+    _label = widget.label;
+    loadNextXNumberOfIssues(_label);
+    print("initState");
+    print(_label);
+  }
+
+  // get the next X number of issues
+  void loadNextXNumberOfIssues(String label, {int number = 10}) {
+    var listener = github.issues
+        .listByRepo(RepositorySlug("flutter", "flutter"), labels: [_label])
+        .take(100)
+        .listen((event) {
+          _issues.add(event);
+          print(_label);
+          // print(event.title);
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+          final index = i ~/ 2;
+          if (_issues.isEmpty || index >= _issues.length) {
+            return Text("No issues yet...");
+          }
+
+          String text = _issues[index].title + " " + index.toString();
+          return Text(text);
+        });
   }
 }
